f1e539bb9048b56d5c0cbe31e53c8ee2bd84067e
Julien Sirocchi <julien.sirocchi@hbc.com>
2018-09-27 15:02:24 +0100

    Ref => Signal for 2.11

--- fs2/src/main/scala-2.11/laserdisc/fs2/fs2.scala
+++ fs2/src/main/scala-2.11/laserdisc/fs2/fs2.scala
@@ -5,11 +5,11 @@ import scala.util.{Failure, Success, Try}
 package object fs2 {
   final type Pipe[F[_], -I, +O] = _root_.fs2.Pipe[F, I, O]
   final type Queue[F[_]]        = _root_.fs2.concurrent.Queue[F, Request[F]]
-  final type Ref[F[_], A]       = _root_.fs2.concurrent.SignallingRef[F, A]
+  final type Signal[F[_], A]    = _root_.fs2.concurrent.SignallingRef[F, A]
   final type Stream[+F[_], +O]  = _root_.fs2.Stream[F, O]
 
   final val Queue  = _root_.fs2.concurrent.Queue
-  final val Ref    = _root_.fs2.concurrent.SignallingRef
+  final val Signal = _root_.fs2.concurrent.SignallingRef
   final val Stream = _root_.fs2.Stream
 
   implicit final class RichTry[+T](val self: Try[T]) extends AnyVal {

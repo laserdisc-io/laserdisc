b9843103bd4a42aeeaaeb11ed033ea7d47b79a28
Julien Sirocchi <julien.sirocchi@hbc.com>
2018-11-06 00:18:11 +0000

    address codacy issues

--- circe/src/test/scala/laserdisc/interop/CirceSpec.scala
+++ circe/src/test/scala/laserdisc/interop/CirceSpec.scala
@@ -3,6 +3,7 @@ package interop
 
 import io.circe.{Decoder, Encoder}
 import io.circe.generic.semiauto._
+import laserdisc.interop.circe._
 import laserdisc.protocol.NonNullBulkString
 import laserdisc.protocol.RESP.bulk
 import org.scalacheck.{Arbitrary, Gen}
@@ -10,8 +11,6 @@ import org.scalatest.prop.PropertyChecks
 import org.scalatest.{MustMatchers, OptionValues, WordSpec}
 
 final class CirceSpec extends WordSpec with MustMatchers with PropertyChecks with OptionValues {
-
-  import circe._
 
   private[this] sealed trait Foo                        extends Product with Serializable
   private[this] final case class Bar(x: Int)            extends Foo
@@ -24,14 +23,15 @@ final class CirceSpec extends WordSpec with MustMatchers with PropertyChecks wit
   private[this] implicit val fooDecoder: Decoder[Foo] = deriveDecoder
   private[this] implicit val fooEncoder: Encoder[Foo] = deriveEncoder
 
-  private[this] implicit val barGen: Gen[Bar] = Arbitrary.arbitrary[Int].map(Bar.apply)
-  private[this] implicit val bazGen: Gen[Baz] = for {
+  private[this] val barGen: Gen[Bar] = Arbitrary.arbitrary[Int].map(Bar.apply)
+  private[this] val bazGen: Gen[Baz] = for {
     s   <- Arbitrary.arbitrary[String]
     foo <- fooGen
   } yield Baz(s, foo)
-  private[this] implicit def fooGen: Gen[Foo] = Gen.oneOf(barGen, bazGen)
+  private[this] def fooGen: Gen[Foo] = Gen.oneOf(barGen, bazGen)
 
-  private[this] implicit def arb[A](implicit A: Gen[A]): Arbitrary[A] = Arbitrary(A)
+  private[this] implicit val barArbitrary: Arbitrary[Bar] = Arbitrary(barGen)
+  private[this] implicit val bazArbitrary: Arbitrary[Baz] = Arbitrary(bazGen)
 
   "Circe interop" when {
 

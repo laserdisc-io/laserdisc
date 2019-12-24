1f22739bb0bfc239a77fffed7ad8132c03880cbd
Filippo Mariotti <filippo.mariotti@gmail.com>
2019-12-25 00:14:27 +0100

    Minor fix on js exception

--- core/.js/src/main/scala/laserdisc/Platform.scala
+++ core/.js/src/main/scala/laserdisc/Platform.scala
@@ -3,5 +3,5 @@ package laserdisc
 private[laserdisc] object Platform {
   abstract class LaserDiscRuntimeError(message: String)              extends RuntimeException(message, null)
   abstract class LaserDiscRespProtocolDecodingError(message: String) extends RuntimeException(message, null)
-  abstract class LaserDiscRespFrameError(message: String)            extends RuntimeException(message, null, true, false)
+  abstract class LaserDiscRespFrameError(message: String)            extends RuntimeException(message, null)
 }

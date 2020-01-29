8f91ae66bdaa4c0c044912123f867f2ab55197b5
Filippo Mariotti <filippo.mariotti@gmail.com>
2020-01-29 00:22:57 +0000

    rempved empty

--- fs2/src/test/scala/laserdisc/fs2/ReadmeExampleSpec.scala
+++ fs2/src/test/scala/laserdisc/fs2/ReadmeExampleSpec.scala
@@ -10,7 +10,6 @@ import scala.concurrent.ExecutionContext
 import scala.concurrent.ExecutionContext.fromExecutor
 
 final class ReadmeExampleSpec extends WordSpecLike with Matchers {
-
   private[this] val ec: ExecutionContext = fromExecutor(new ForkJoinPool())
 
   private[this] implicit val timer: Timer[IO]               = IO.timer(ec)

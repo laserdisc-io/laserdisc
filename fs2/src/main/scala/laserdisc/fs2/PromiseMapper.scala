b1757dbf0f0b08c26bb0e541cb8b084d0357e2d0
Julien Jean Paul Sirocchi <julien.sirocchi@gmail.com>
2021-03-29 12:48:10 +0100

    Update PromiseMapper.scala

--- fs2/src/main/scala/laserdisc/fs2/PromiseMapper.scala
+++ fs2/src/main/scala/laserdisc/fs2/PromiseMapper.scala
@@ -1,14 +1,13 @@
 package laserdisc
 package fs2
 
+import cats.effect.{Concurrent, Deferred, Temporal}
 import cats.effect.syntax.concurrent._
-import cats.effect.Concurrent
 import cats.syntax.flatMap._
 import cats.syntax.monadError._
 import shapeless.Poly1
 
 import scala.concurrent.TimeoutException
-import cats.effect.{ Deferred, Temporal }
 
 object PromiseMapper extends Poly1 {
   private[this] final def mapper[F[_]: Concurrent: Temporal, A](protocol: Protocol.Aux[A]): Env[F] => F[Maybe[A]] = { case (queue, duration) =>
